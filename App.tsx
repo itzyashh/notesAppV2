dd41778e12a6737574131852a28b64100265f67e
Yash <yashjadhav1411@rocketmail.com>
2023-05-01 13:32:35 +0530

    Inital commit with navigation

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StatusBar } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar  backgroundColor="#000"  />
+    </View>
+  )
+}
+
+export default App
